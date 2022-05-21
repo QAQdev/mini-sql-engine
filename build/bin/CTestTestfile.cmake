398aa9eceee81b35128387fb03df44e24f60caef
kenshin <73159641+QAQdev@users.noreply.github.com>
2022-05-21 23:02:54 +0800

    Delete build directory

--- build/bin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/MiniSQL_v3/src
-# Build directory: /root/MiniSQL_v3/build/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
